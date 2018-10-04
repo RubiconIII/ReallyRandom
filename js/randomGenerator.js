4a720b6bd85a5f27699e83c83cc0fc6bc9cacbc4
root <root@Curtis-MateBook-X-Pro.localdomain>
2018-10-03 23:18:56 -0700

    Added random generation based on input seed. Fixed order of title bar control

---
+++ js/randomGenerator.js
@@ -0,0 +1,14 @@
+function getRandomString() {
+    alert(randomString(document.getElementById("rString").value)); //send an alert with the generated string
+}
+
+var randomString = function randomString(inputString) { 
+	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz1234567890"; //list of avaliable chars
+	var stringLength = 8; //password length
+	var random = ""; //init random string
+	for (var i=0; i<stringLength; i++) { //append for as long as the stringLength
+		var rnum = Math.floor(Math.random(inputString) * (chars.length - 1)); //get a random num from the chars
+		random += chars.charAt(rnum); //random equals itself plus random char
+	}
+    return random //return the random string
+}
