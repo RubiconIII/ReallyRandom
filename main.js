f09271e07f63e12ac5d4f114049781a9f78c6951
root <root@Curtis-MateBook-X-Pro.localdomain>
2018-10-04 10:22:23 -0700

    Fixed random character list and added Inter UI

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, frame: false})
+  mainWindow = new BrowserWindow({width: 800, height: 300, frame: false, backgroundColor:"#000000"})
 
   // and load the index.html of the app.
   mainWindow.loadFile('html/index.html')
